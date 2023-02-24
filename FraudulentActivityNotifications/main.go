dc4c4e66644a0a87b6be0e9f938becac4c3a2ed7
Pick1a1username <xscreensaver@outlook.com>
2023-02-24 18:47:12 +0900

    20230224_1

--- FraudulentActivityNotifications/main.go
+++ FraudulentActivityNotifications/main.go
@@ -41,3 +41,14 @@ func getMedianInt32(arr []int32) float32 {
 		return float32(arr[l/2])
 	}
 }
+
+func bubble(arr *[]int32) {
+	// Bubble sort
+	for i := 0; i < len(*arr)-1; i++ {
+		for j := i + 1; j < len(*arr); j++ {
+			if (*arr)[i] > (*arr)[j] {
+				(*arr)[i], (*arr)[j] = (*arr)[j], (*arr)[i]
+			}
+		}
+	}
+}
--- FraudulentActivityNotifications/main_test.go
+++ FraudulentActivityNotifications/main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_activityNotifications(t *testing.T) {
 	testCases := []struct {
@@ -34,6 +37,41 @@ func Test_activityNotifications(t *testing.T) {
 			result := activityNotifications(testCase.expenditure, testCase.d)
 			if result != testCase.answer {
 				t.Errorf("result: %d answer: %d", result, testCase.answer)
+			}
+		})
+	}
+}
+
+func Test_bubble(t *testing.T) {
+	testCases := []struct {
+		description string
+		arr         []int32
+		answer      []int32
+	}{
+		{
+			description: "TestCase 1",
+			arr:         []int32{2, 3, 4, 2, 3, 6, 8, 4, 5},
+			answer:      []int32{2, 2, 3, 3, 4, 4, 5, 6, 8},
+		},
+		{
+			description: "TestCase 2",
+			arr:         []int32{1, 2, 3, 4, 4},
+			answer:      []int32{1, 2, 3, 4, 4},
+		},
+		{
+			description: "TestCase 3",
+			arr:         []int32{5, 4, 3, 2, 1},
+			answer:      []int32{1, 2, 3, 4, 5},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.description, func(t *testing.T) {
+			copiedArr := []int32{}
+			copiedArr = append(copiedArr, testCase.arr...)
+			bubble(&copiedArr)
+			if !reflect.DeepEqual(copiedArr, testCase.answer) {
+				t.Errorf("result: %v answer: %v", copiedArr, testCase.answer)
 			}
 		})
 	}
